f15811c48c3228f9616d1eecdf6f957207088873
Vitali Arystau <vitarisun69@gmail.com>
2022-09-25 20:18:05 +0300

    Click Buttons

--- src/components/energy-resources-filter/energy-resources-filter.js
+++ src/components/energy-resources-filter/energy-resources-filter.js
@@ -7,15 +7,41 @@ class EnergyResourcesFilter extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      werV: false,
       resources: false,
+
+      // dataButtons: [
+
+      //   {energRes: electro, stateEnergRes: false},
+      //   {energRes:warm, stateEnergRes: false},
+      //   {energRes:gas, stateEnergRes: false},
+      //   {energRes:firewood, stateEnergRes: false},
+      //   {energRes:briqutte, stateEnergRes: false},
+      //   {energRes:water, stateEnergRes: false},
+      //   {energRes:werV, stateEnergRes: false},
+      //   {energRes:resources, stateEnergRes: false},
+      // ]
     };
 
     this.onResources = this.onResources.bind(this);
+    this.onwerV = this.onwerV.bind(this);
   }
+
+  // const onResources = dataButtons.map(({energRes, }) => {
+  //       this.setState(({ energRes }) => ({
+  //     energRes: !energRes,
+  //   }));
+  // })
 
   onResources = () => {
     this.setState(({ resources }) => ({
       resources: !resources,
+    }));
+  };
+
+  onwerV = () => {
+    this.setState(({ werV }) => ({
+      werV: !werV,
     }));
   };
 
@@ -25,44 +51,98 @@ class EnergyResourcesFilter extends Component {
       menuEnergyResources = <EnergyResources />;
     }
 
+    const buttonsEnergyResourcesFilter = [
+      { name: 'Eectro', label: 'Электоэнергия' },
+      { name: 'Warm', label: 'Теплоэнергия' },
+      { name: 'Gas', label: 'Газ' },
+      { name: 'Firewood', label: 'Дрова' },
+      { name: 'Briqutte', label: 'Торфобрикет' },
+      { name: 'Water', label: 'Вода' },
+      { name: 'WerV', label: 'Объемы отпуска ТЭР' },
+      { name: 'Resources', label: 'Отчеты' },
+    ];
+
+    const buttons = buttonsEnergyResourcesFilter.map(({ name, label }) => {
+      // let click = this.props;
+      // if (name === 'Resources') {
+      //   click = this.onResource;
+      // } else {
+      //   click = false;
+      // }
+      function click(e) {
+        e.preventDefault();
+
+        if (name === 'Resources') {
+          console.log(name);
+        }
+        if (name === 'WerV') {
+          console.log(name);
+        }
+        //
+
+        // if (name === 'Resources') {
+        //  oClick = this.onResource
+        // }
+      }
+
+      return (
+        <button
+          className="btn btn-outline-light"
+          type="button"
+          key={name}
+          onClick={click}
+        >
+          {label}
+        </button>
+      );
+    });
+
     return (
       <div>
         <div className="btn-group">
-          <button className="btn btn-outline-light" type="button">
+          {buttons}
+          {/* <button className="btn btn-outline-light" type="button" name="Eectro">
             Электоэнергия
           </button>
 
-          <button className="btn btn-outline-light" type="button">
+          <button className="btn btn-outline-light" type="button" name="Warm">
             Теплоэнергия
           </button>
 
-          <button className="btn btn-outline-light" type="button">
+          <button className="btn btn-outline-light" type="button" name="Gas">
             Газ
           </button>
 
-          <button className="btn btn-outline-light" type="button">
-            Дрова
-          </button>
-
-          <button className="btn btn-outline-light" type="button">
-            Торфобрикет
-          </button>
-
-          <button className="btn btn-outline-light" type="button">
-            Вода
-          </button>
-          <button className="btn btn-outline-light" type="button">
-            Объемы отпуска ТЭР
-          </button>
-
-          {/* <button className="btn btn-outline-light" type="button"> */}
           <button
             className="btn btn-outline-light"
             type="button"
+            name="Firewood"
+          >
+            Дрова
+          </button>
+
+          <button
+            className="btn btn-outline-light"
+            type="button"
+            name="Briqutte"
+          >
+            Торфобрикет
+          </button>
+
+          <button className="btn btn-outline-light" type="button" name="Water">
+            Вода
+          </button>
+          <button className="btn btn-outline-light" type="button" name="WerV">
+            Объемы отпуска ТЭР
+          </button> */}
+          {/* <button
+            className="btn btn-outline-light"
+            type="button"
+            name="Resources"
             onClick={this.onResources}
           >
             Отчеты
-          </button>
+          </button> */}
         </div>
 
         <div>{menuEnergyResources}</div>
