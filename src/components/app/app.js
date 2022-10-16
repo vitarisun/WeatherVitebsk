b14a0f80af375171f608ab1e1651f8e4365e2a59
Vitali Arystau <vitarisun69@gmail.com>
2022-10-16 10:44:10 +0300

    Hide-Show energy-resources in filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,51 +1,70 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 
 import AppInfo from '../app-info/app-info';
 import EnergyResourcesFilter from '../energy-resources-filter/energy-resources-filter';
 // import EnergyResources from '../energy-resources/energy-resources';
-import ElectricityList from '../electricity-list/electricity-list';
+// import ElectricityList from '../electricity-list/electricity-list';
 import ElectroAddForm from '../electro-add-form/electro-add-form';
-import WarmInfo from '../warm-info/warm-info';
-import GasInfo from '../gas-info/gas-info';
-import WaterInfo from '../water-info/water-info';
-import BriqnetteFirewoodInfo from '../briqnette-firewood-info/briqnette-firewood-info';
+// import WarmInfo from '../warm-info/warm-info';
+// import GasInfo from '../gas-info/gas-info';
+// import WaterInfo from '../water-info/water-info';
+// import BriqnetteFirewoodInfo from '../briqnette-firewood-info/briqnette-firewood-info';
 
 import './app.css';
 
-// function App(props) {
-//
+function App(props) {
+  //
 
-//   let value = true;
-//   let valueMenu = true;
-//   let valueElectricity = true;
-//   let valueElectroAddForm = true;
-//   let valueEnergyResources = true;
+  return (
+    <div className="app">
+      <AppInfo />
+      <div>
+        <EnergyResourcesFilter />
+      </div>
+      {/* <ElectricityList  /> */}
+      {/* {menuElectro} */}
+      {/* <WarmInfo /> */}
+      {/* <GasInfo /> */}
+      {/* <WaterInfo data={dataWarm} /> */}
+      {/* <WaterInfo /> */}
+      {/* <BriqnetteFirewoodInfo /> */}
+      <div>
+        <ElectroAddForm />
+      </div>
+    </div>
+  );
 
-//   let appInfo = null;
-//   let menuInfo = null;
-//   let menuElectro = null;
-//   let menuElectroAddForm = null;
-// let menuEnergyResources = null;
+  //   let value = true;
+  //   let valueMenu = true;
+  //   let valueElectricity = true;
+  //   let valueElectroAddForm = true;
+  //   let valueEnergyResources = true;
 
-//   if (value) {
-//     appInfo = <AppInfo />;
-//   }
+  //   let appInfo = null;
+  //   let menuInfo = null;
+  //   let menuElectro = null;
+  //   let menuElectroAddForm = null;
+  // let menuEnergyResources = null;
 
-//   if (valueMenu) {
-//     menuInfo = <EnergyResourcesFilter />;
-//   }
+  //   if (value) {
+  //     appInfo = <AppInfo />;
+  //   }
 
-//   if (valueEnergyResources) {
-//     menuEnergyResources = <EnergyResources />;
-//   }
+  //   if (valueMenu) {
+  //     menuInfo = <EnergyResourcesFilter />;
+  //   }
 
-//   if (valueElectricity) {
-//     menuElectro = <ElectricityList />;
-//   }
+  //   if (valueEnergyResources) {
+  //     menuEnergyResources = <EnergyResources />;
+  //   }
 
-//   if (valueElectroAddForm) {
-//     menuElectroAddForm = <ElectroAddForm />;
-//   }
+  //   if (valueElectricity) {
+  //     menuElectro = <ElectricityList />;
+  //   }
+
+  //   if (valueElectroAddForm) {
+  //     menuElectroAddForm = <ElectroAddForm />;
+}
 
 //   return (
 //     <div className="app">
@@ -58,76 +77,43 @@ import './app.css';
 //   );
 // }
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataElectro: [
-        {
-          i: 1,
-          branch: 'Административное помещение филиала «Витебскоблгидромет»',
-          treatyElectro: '01-550',
-          typeCounter: 'СС-301-10.1/U/1/P(L)K',
-          numberCounter: 16066232,
-          startCounter: 193001,
-          endCounter: 193030,
-          transformer: '0',
-          coefficient: 1,
-          electroResult: 29,
-          id: 1,
-        },
-        {
-          i: 2,
-          branch: 'ДМРЛ (аэропорт «Витебск»), Ввод № 1',
-          treatyElectro: '01-550',
-          typeCounter: 'СС-301-5.1/U',
-          numberCounter: 18087619,
-          startCounter: 3277.52,
-          endCounter: 3363.4,
-          transformer: '150/5',
-          coefficient: 30,
-          electroResult: 2576,
-          id: 2,
-        },
-      ],
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//     // this.hideElectro = this.hidEelectro.bind(this);
+//     // this.onElectro = this.onElectro.bind(this);
+//   }
 
-      // electro: false,
-    };
-    // this.electro = this.electro.bind(this);
-  }
+// hidEelectro = (name) => {
+//   console.log(this.state);
+// };
 
-  // onElectro = () => {
-  //   this.setState(({ electro }) => ({
-  //     electro: !electro,
-  //   }));
-  // };
+// render() {
+//   // let menuElectro = null;
+//   // if (this.state.electro) {
+//   //   menuElectro = <ElectricityList data={this.state.dataElectro} />;
+//   // }
 
-  render() {
-    // let menuElectro = null;
-    // if (this.state.electro) {
-    //   menuElectro = <ElectricityList data={this.state.dataElectro} />;
-    // }
-
-    return (
-      <div className="app">
-        <AppInfo />
-        <div>
-          <EnergyResourcesFilter />
-        </div>
-        <div></div>
-        <ElectricityList data={this.state.dataElectro} />
-        {/* {menuElectro} */}
-        <WarmInfo />
-        <GasInfo />
-        {/* <WaterInfo data={dataWarm} /> */}
-        <WaterInfo />
-        <BriqnetteFirewoodInfo />
-        <div>
-          <ElectroAddForm />
-        </div>
-      </div>
-    );
-  }
-}
+//   return (
+//     <div className="app">
+//       <AppInfo />
+//       <div>
+//         <EnergyResourcesFilter />
+//       </div>
+//       {/* <ElectricityList  /> */}
+//       {/* {menuElectro} */}
+//       <WarmInfo />
+//       <GasInfo />
+//       {/* <WaterInfo data={dataWarm} /> */}
+//       <WaterInfo />
+//       <BriqnetteFirewoodInfo />
+//       <div>
+//         <ElectroAddForm />
+//       </div>
+//     </div>
+//   );
+// }
+// }
 
 export default App;
--- src/components/energy-resources-filter/energy-resources-filter.js
+++ src/components/energy-resources-filter/energy-resources-filter.js
@@ -1,5 +1,10 @@
 import { Component } from 'react';
 import EnergyResources from '../energy-resources/energy-resources';
+import ElectricityList from '../electricity-list/electricity-list';
+import WarmInfo from '../warm-info/warm-info';
+import GasInfo from '../gas-info/gas-info';
+import WaterInfo from '../water-info/water-info';
+import BriqnetteFirewoodInfo from '../briqnette-firewood-info/briqnette-firewood-info';
 
 import './energy-resources-filter.css';
 
@@ -7,16 +12,83 @@ class EnergyResourcesFilter extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      dataElectro: [
+        {
+          i: 1,
+          branch: 'Административное помещение филиала «Витебскоблгидромет»',
+          treatyElectro: '01-550',
+          typeCounter: 'СС-301-10.1/U/1/P(L)K',
+          numberCounter: 16066232,
+          startCounter: 193001,
+          endCounter: 193030,
+          transformer: '0',
+          coefficient: 1,
+          electroResult: 29,
+          id: 1,
+        },
+        {
+          i: 2,
+          branch: 'ДМРЛ (аэропорт «Витебск»), Ввод № 1',
+          treatyElectro: '01-550',
+          typeCounter: 'СС-301-5.1/U',
+          numberCounter: 18087619,
+          startCounter: 3277.52,
+          endCounter: 3363.4,
+          transformer: '150/5',
+          coefficient: 30,
+          electroResult: 2576,
+          id: 2,
+        },
+      ],
       electro: false,
+      warm: false,
+      gas: false,
+      water: false,
+      firewoodBriqutte: false,
       werV: false,
       resources: false,
     };
 
-    this.onwerV = this.onwerV.bind(this);
+    this.onElectro = this.onElectro.bind(this);
+    this.onWarm = this.onWarm.bind(this);
+    this.onGas = this.onGas.bind(this);
+    this.onWater = this.onWater.bind(this);
+    this.onFirewoodBriqutte = this.onFirewoodBriqutte.bind(this);
+    // this.onwerV = this.onwerV.bind(this);
     this.onResources = this.onResources.bind(this);
   }
 
-  onwerV = () => {
+  onElectro = () => {
+    this.setState(({ electro }) => ({
+      electro: !electro,
+    }));
+  };
+
+  onWarm = () => {
+    this.setState(({ gas }) => ({
+      gas: !gas,
+    }));
+  };
+
+  onGas = () => {
+    this.setState(({ gas }) => ({
+      gas: !gas,
+    }));
+  };
+
+  onWater = () => {
+    this.setState(({ water }) => ({
+      water: !water,
+    }));
+  };
+
+  onFirewoodBriqutte = () => {
+    this.setState(({ firewoodBriqutte }) => ({
+      firewoodBriqutte: !firewoodBriqutte,
+    }));
+  };
+
+  onWerV = () => {
     this.setState(({ werV }) => ({
       werV: !werV,
     }));
@@ -29,19 +101,29 @@ class EnergyResourcesFilter extends Component {
   };
 
   render() {
-    let menuEnergyResources = null;
-    if (this.state.resources) {
-      menuEnergyResources = <EnergyResources />;
-    }
+    // const { resources } = this.state;
+    // let menuEnergyResources = null;
+    // let menuElectroList = null;
+
+    // if (resources) {
+    //   menuEnergyResources = <EnergyResources />;
+    // }
+    // if (electro) {
+    //   // menuElectroList = <ElectricityList data={this.state.dataElectro} />;
+    //   menuElectroList = <ElectricityList />;
+    // }
 
     const buttonsEnergyResourcesFilter = [
-      { name: 'Eectro', label: 'Электоэнергия', click: this.electro },
-      { name: 'Warm', label: 'Теплоэнергия' },
-      { name: 'Gas', label: 'Газ' },
-      { name: 'Firewood', label: 'Дрова' },
-      { name: 'Briqutte', label: 'Торфобрикет' },
-      { name: 'Water', label: 'Вода' },
-      { name: 'WerV', label: 'Объемы отпуска ТЭР', click: this.onwerV },
+      { name: 'Electro', label: 'Электоэнергия', click: this.onElectro },
+      { name: 'Warm', label: 'Теплоэнергия', click: this.onWarm },
+      { name: 'Gas', label: 'Газ', click: this.onGas },
+      { name: 'Water', label: 'Вода', click: this.onWater },
+      {
+        name: 'FirewoodBriqutte',
+        label: 'Дрова/Торфобрикет',
+        click: this.onFirewoodBriqutte,
+      },
+      { name: 'WerV', label: 'Объемы отпуска ТЭР', click: this.onWerV },
       { name: 'Resources', label: 'Отчеты', click: this.onResources },
     ];
 
@@ -63,7 +145,18 @@ class EnergyResourcesFilter extends Component {
     return (
       <div>
         <div className="btn-group">{buttons}</div>
-        <div>{menuEnergyResources}</div>
+        {this.state.resources ? <EnergyResources /> : null}
+        <div>
+          {this.state.electro ? (
+            <ElectricityList data={this.state.dataElectro} />
+          ) : null}
+        </div>
+        <div>{this.state.warm ? <WarmInfo /> : null}</div>
+        <div>{this.state.gas ? <GasInfo /> : null}</div>
+        <div>{this.state.water ? <WaterInfo /> : null}</div>
+        <div>
+          {this.state.firewoodBriqutte ? <BriqnetteFirewoodInfo /> : null}
+        </div>
       </div>
     );
   }
